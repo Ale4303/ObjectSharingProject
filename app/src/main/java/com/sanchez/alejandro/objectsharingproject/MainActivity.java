d524e71c9b70181c9b3b7dda2aa4ab6791732c0d
Ale4303 <Ale_alejandro.sanchez@hotmail.com>
2017-02-09 13:19:41 +0100

    just a test

--- app/src/main/java/com/sanchez/alejandro/objectsharingproject/MainActivity.java
+++ app/src/main/java/com/sanchez/alejandro/objectsharingproject/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-//test JUST A TEST
+//test AS
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
